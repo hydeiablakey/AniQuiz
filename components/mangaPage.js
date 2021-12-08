6262d6db11b74aba5110f255e3cc871d44d4f82e
Hydeia <blakeyhydeia@gmail.com>
2021-12-07 19:08:35 -0500

    get manga image to show on webpage

--- components/mangaPage.js
+++ components/mangaPage.js
@@ -14,12 +14,23 @@ guessMangaBtn && guessMangaBtn.addEventListener("click", guessMangaPage);
 const getMangaImg = async () => {
 try {
     const response = await axios.get(`https://api.jikan.moe/v3/manga/1/pictures`)
-    console.log(response.data.pictures[0].large); 
-    console.log('th')
+    console.log(response.data.pictures[0].large);
+    console.log('help me'); 
+    return response.data.pictures[0].large;  
+    // console.log('th')
 
 } catch (err) {
     console.log(err)
 }
 }
 
-getMangaImg(); 
+const createMangaImg = async () => {
+    const imgData = await getMangaImg(); 
+    const parentDiv = document.getElementById('mangaColumn')
+    const mangaImg = document.createElement('img'); 
+    mangaImg.src =`${imgData}`
+    parentDiv.appendChild(mangaImg); 
+
+}
+
+createMangaImg(); 
